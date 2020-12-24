94236048fe316959b97879c071112d7d278b618c
mkuliberda <mkuliberda@gmail.com>
2020-12-24 13:01:19 +0100

    initial commit

---
+++ MDK-ARM/AR_50_H_cube/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+ar_50_h_cube\startup_stm32f407xx.o: startup_stm32f407xx.s
---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'AR_50_H_cube' 
+ * Target:  'AR_50_H_cube' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
